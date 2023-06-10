b8aae34d35ba8af0db56ec5fc766c2dd7e92a4ff
Hsouzasantos <hugosantos354@gmail.com>
2023-06-10 16:03:16 -0300

    Criacao das rotas de busca e criacao

--- back/src/router/route.ts
+++ back/src/router/route.ts
@@ -3,13 +3,80 @@ import { prisma } from '../prisma';
 
 const route = Router();
 
+//Busca
 route.get("/centrais", async (req, res) => {
 
-    const centrais = await prisma.central.findMany({
-
-    })
+    const centrais = await prisma.central.findMany()
+    console.log(centrais)
 
     return res.status(200).json(centrais)
+});
+
+route.get("/catadores",async (req, res) => {
+
+    const catadores = await prisma.catador.findMany()
+    console.log(catadores)
+
+    return res.status(200).json(catadores)    
+});
+
+route.get("/quadrantes", async (req, res) => {
+
+    const quadrantes = await prisma.quadrante.findMany()
+    console.log(quadrantes)
+
+    return res.status(200).json(quadrantes)
+});
+
+//Cadastro
+route.post("/centrais", async (req, res) => {
+
+    const body: any = req.body;
+
+    const central = await prisma.central.create({
+        data:{
+            razao_social: body.razao_social,
+            CEP: body.CEP,
+            endereco: body.endereco,
+            numero: body.numero,
+            CNPJ: body.CNPJ
+        }
+    })
+
+    return res.status(200).json(central)
+});
+
+route.post("/quadrantes", async (req, res) => {
+
+    const body = req.body;
+
+    const quadrante = await prisma.quadrante.create({
+        data:{
+            quadrante: body.quadrante,
+            CEP: body.CEP,
+            pontuacao: body.pontuacao
+        }
+    })
+    return res.status(200).json(quadrante)
+});
+
+route.post("/catadores", async (req, res) => {
+    
+    const body = req.body;
+
+    const catador = await prisma.catador.create({
+        data:{
+            nome: body.nome,
+            CPF: body.CPF,
+            CEP: body.CEP,
+            endereco: body.endereco,
+            numero: body.numero,
+            centralId: body.centralId,
+            quadranteId: body.quadranteId
+        }
+    })
 })
+
+//Update
 
 export default route;
--- back/src/server.ts
+++ back/src/server.ts
@@ -4,11 +4,11 @@ import route from './router/route';
 
 
 const hostname = "http://localhost:";
-const port = "5000";
+const port = "5050";
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: 'http://localhost:5173'}));
+//app.use(cors({origin: 'http://localhost:5173'}));
 
 app.use(route)
 
