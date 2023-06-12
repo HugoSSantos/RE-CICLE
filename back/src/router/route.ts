cd1f04ffb2e9631fc5823c48179be0268fcb4e14
Hsouzasantos <hugosantos354@gmail.com>
2023-06-12 00:55:02 -0300

    Criação das rotas do stacks centrais, quadrantes e catadores

--- back/src/router/route.ts
+++ back/src/router/route.ts
@@ -3,7 +3,7 @@ import { prisma } from '../prisma';
 
 const route = Router();
 
-//Busca
+//Busca geral
 route.get("/centrais", async (req, res) => {
 
     const centrais = await prisma.central.findMany()
@@ -26,6 +26,68 @@ route.get("/quadrantes", async (req, res) => {
     console.log(quadrantes)
 
     return res.status(200).json(quadrantes)
+});
+
+// Busca individual
+route.get("/centrais/:id", async (req, res) =>{
+    const centralId = req.params.id;
+
+    const central = await prisma.central.findMany({
+        select:{
+            id_central: true,
+            razao_social: true,
+            CNPJ: true,
+            CEP: true,
+            catador: true,
+            endereco: true,
+            numero: true
+        },
+        where:{
+            id_central: centralId
+        }
+    })
+
+    return res.status(200).json(central)
+});
+
+route.get("/catadores/:id", async (req, res) => {
+
+    const catadorId = req.params.id;
+
+    const catador = await prisma.catador.findMany({
+        select:{
+            id_catador: true,
+            nome: true,
+            centralId: true,
+            quadranteId: true
+        },
+        where:{
+            id_catador: catadorId
+        }
+    })
+
+    return res.status(200).json(catador)
+
+});
+
+route.get("quadrantes/:id", async (req, res) => {
+    
+    const quadranteId = req.params.id;
+
+    const quadrante = await prisma.quadrante.findMany({
+        select:{
+            id_quadrante: true,
+            quadrante: true,
+            CEP: true,
+            pontuacao: true,
+            catador: true
+        },
+        where: {
+            id_quadrante: quadranteId
+        }
+    })
+
+    return res.status(200).json(quadrante)
 });
 
 //Cadastro
